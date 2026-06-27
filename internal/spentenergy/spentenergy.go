d2fb796ee3d5cf0deae081a8ab9a7502a634cb34
VellyJoy <mak@MacBook-Pro-Mak.local>
2026-06-27 19:01:51 +0300

    final-5

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -2,6 +2,7 @@ package spentenergy
 
 import (
 	"time"
+	"errors"
 )
 
 // Основные константы, необходимые для расчетов.
@@ -14,16 +15,63 @@ const (
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 {
+		return 0, errors.New("invalid steps")
+	}
+	if weight <= 0 {
+		return 0, errors.New("invalid weight")
+	}
+	if height <= 0 {
+		return 0, errors.New("invalid height")
+	}
+	if duration <= 0 {
+		return 0, errors.New("invalid duration")
+	}
+
+	distance := Distance(steps, height)
+	calories := distance * weight * walkingCaloriesCoefficient
+
+	return calories, nil
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 {
+		return 0, errors.New("invalid steps")
+	}
+	if weight <= 0 {
+		return 0, errors.New("invalid weight")
+	}
+	if height <= 0 {
+		return 0, errors.New("invalid height")
+	}
+	if duration <= 0 {
+		return 0, errors.New("invalid duration")
+	}
+
+	speed := MeanSpeed(steps, height, duration)
+	calories := speed * weight * duration.Hours()
+
+	return calories, nil
+
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
+	if steps <= 0 || height <= 0 || duration <= 0 {
+		return 0
+	}
+
+	return Distance(steps, height) / duration.Hours()
 }
 
 func Distance(steps int, height float64) float64 {
 	// TODO: реализовать функцию
+	if steps <= 0 || height <= 0 {
+		return 0
+	}
+
+	stepLength := height * stepLengthCoefficient
+
+	return float64(steps) * stepLength / mInKm
 }
